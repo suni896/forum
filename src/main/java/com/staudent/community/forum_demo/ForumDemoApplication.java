8238684e39248035c3d260b8222153b8b92632be
suni <valerie_95@163.com>
2024-08-29 00:25:12 +0800

    add index and put static

--- src/main/java/com/staudent/community/forum_demo/ForumDemoApplication.java
+++ src/main/java/com/staudent/community/forum_demo/ForumDemoApplication.java
@@ -8,6 +8,6 @@ public class ForumDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ForumDemoApplication.class, args);
+        System.out.println("Hello, World!");
     }
-
 }
--- src/main/java/com/staudent/community/forum_demo/controller/forumController.java
+++ src/main/java/com/staudent/community/forum_demo/controller/forumController.java
@@ -1,4 +1,15 @@
 package com.staudent.community.forum_demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class forumController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
